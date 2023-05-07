2f5dd00c93ccd77322be82595258bf05e3f1383c
Shemrock3267 <dev.oliinyk@gmail.com>
2023-05-07 11:58:20 +0300

    added several hooks

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import useToggle from './hooks/useToggle';
+import useTimeout from './hooks/useTimeout';
+import useDebounce from './hooks/useDebounce';
+import useWindowSize from './hooks/useWindowSize';
+import useActiveElement from './hooks/useActiveElement';
+import useOutSideClick from './hooks/useOutsideClick';
+
+export {
+  useToggle,
+  useTimeout,
+  useDebounce,
+  useWindowSize,
+  useActiveElement,
+  useOutSideClick,
+};
